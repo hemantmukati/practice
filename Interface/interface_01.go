ffba8a0599c8b9981e0e56f00a992d2ad3387081
Hemant <hmukati061094@gmail.com>
2021-10-12 15:02:44 +0530

    intial commit

--- Interface/interface_01.go
+++ Interface/interface_01.go
@@ -1,1 +1,30 @@
-package Interface
+package main
+import "fmt"
+
+type person interface {
+	specification()
+}
+
+type hemant struct {
+	surname string
+	age int
+	height float64
+}
+ func(h hemant) specification (){
+	 fmt.Println(h)
+ }
+ func what(x person ) (y interface{}) {
+	 return y
+ }
+
+func main (){
+	h:= hemant {
+		surname : "mukati" ,
+		age : 25 ,
+		height: 5.3 ,
+	}
+	var p person = h
+	 h.specification()
+     fmt.Println(what(p))
+
+}
