9a4dd774852cbe68966926bec89769427a4583dc
Hemant <hmukati061094@gmail.com>
2022-03-07 18:22:51 +0530

    intial commit

---
+++ methods/method01.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	r := register{
+		name:     "hemant",
+		subject:  "cse",
+		mobile:   123,
+		password: "h@123",
+	}
+
+	r.registerUser()
+	fmt.Println("details of user:", r)
+}
+
+type register struct {
+	name     string
+	subject  string
+	mobile   int
+	password string
+}
+
+func (x register) registerUser() {
+
+	fmt.Println("user name is :", x.name)
+	fmt.Println("user mobile no. is :", x.mobile)
+}
